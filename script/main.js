37ad619f37bcafec23c27afb086df9f2fee56a82
Arun Redhu <arun.redhu05@gmail.com>
2020-07-27 20:31:05 +0530

    BLub added

--- script/main.js
+++ script/main.js
@@ -1,292 +1,291 @@
 // Animation Timeline
 const animationTimeline = () => {
   // Spit chars that needs to be animated individually
-  const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
-  const hbd = document.getElementsByClassName("wish-hbd")[0];
+  const textBoxChars = document.getElementsByClassName('hbd-chatbox')[0];
+  const hbd = document.getElementsByClassName('wish-hbd')[0];
 
   textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
-    .split("")
-    .join("</span><span>")}</span`;
+    .split('')
+    .join('</span><span>')}</span`;
 
   hbd.innerHTML = `<span>${hbd.innerHTML
-    .split("")
-    .join("</span><span>")}</span`;
+    .split('')
+    .join('</span><span>')}</span`;
 
   const ideaTextTrans = {
     opacity: 0,
     y: -20,
     rotationX: 5,
-    skewX: "15deg"
+    skewX: '15deg',
   };
 
   const ideaTextTransLeave = {
     opacity: 0,
     y: 20,
     rotationY: 5,
-    skewX: "-15deg"
+    skewX: '-15deg',
   };
 
   const tl = new TimelineMax();
 
-  tl
-    .to(".container", 0.1, {
-      visibility: "visible"
-    })
-    .from(".one", 0.7, {
+  tl.to('.container', 0.1, {
+    visibility: 'visible',
+  })
+    .from('.one', 0.7, {
       opacity: 0,
-      y: 10
+      y: 10,
     })
-    .from(".two", 0.4, {
+    .from('.two', 0.4, {
       opacity: 0,
-      y: 10
+      y: 10,
     })
     .to(
-      ".one",
+      '.one',
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
-      "+=2.5"
+      '+=2.5'
     )
     .to(
-      ".two",
+      '.two',
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
-      "-=1"
+      '-=1'
     )
-    .from(".three", 0.7, {
+    .from('.three', 0.7, {
       opacity: 0,
-      y: 10
+      y: 10,
       // scale: 0.7
     })
     .to(
-      ".three",
+      '.three',
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
-      "+=2"
+      '+=2'
     )
-    .from(".four", 0.7, {
+    .from('.four', 0.7, {
       scale: 0.2,
-      opacity: 0
+      opacity: 0,
     })
-    .from(".fake-btn", 0.3, {
+    .from('.fake-btn', 0.3, {
       scale: 0.2,
-      opacity: 0
+      opacity: 0,
     })
     .staggerTo(
-      ".hbd-chatbox span",
+      '.hbd-chatbox span',
       0.5,
       {
-        visibility: "visible"
+        visibility: 'visible',
       },
       0.05
     )
-    .to(".fake-btn", 0.1, {
-      backgroundColor: "rgb(127, 206, 248)"
+    .to('.fake-btn', 0.1, {
+      backgroundColor: '#ce93d8',
     })
     .to(
-      ".four",
+      '.four',
       0.5,
       {
         scale: 0.2,
         opacity: 0,
-        y: -150
+        y: -150,
       },
-      "+=0.7"
+      '+=0.7'
     )
-    .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
+    .from('.idea-1', 0.7, ideaTextTrans)
+    .to('.idea-1', 0.7, ideaTextTransLeave, '+=1.5')
+    .from('.idea-2', 0.7, ideaTextTrans)
+    .to('.idea-2', 0.7, ideaTextTransLeave, '+=1.5')
+    .from('.idea-3', 0.7, ideaTextTrans)
+    .to('.idea-3 strong', 0.5, {
       scale: 1.2,
       x: 10,
-      backgroundColor: "rgb(21, 161, 237)",
-      color: "#fff"
+      backgroundColor: '#9c27b0',
+      color: '#fff',
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .to('.idea-3', 0.7, ideaTextTransLeave, '+=1.5')
+    .from('.idea-4', 0.7, ideaTextTrans)
+    .to('.idea-4', 0.7, ideaTextTransLeave, '+=1.5')
     .from(
-      ".idea-5",
+      '.idea-5',
       0.7,
       {
         rotationX: 15,
         rotationZ: -10,
-        skewY: "-5deg",
+        skewY: '-5deg',
         y: 50,
         z: 10,
-        opacity: 0
+        opacity: 0,
       },
-      "+=0.5"
+      '+=0.5'
     )
     .to(
-      ".idea-5 span",
+      '.idea-5 span',
       0.7,
       {
         rotation: 90,
-        x: 8
+        x: 8,
       },
-      "+=0.4"
+      '+=0.4'
     )
     .to(
-      ".idea-5",
+      '.idea-5',
       0.7,
       {
         scale: 0.2,
-        opacity: 0
+        opacity: 0,
       },
-      "+=2"
+      '+=2'
     )
     .staggerFrom(
-      ".idea-6 span",
+      '.idea-6 span',
       0.8,
       {
         scale: 3,
         opacity: 0,
         rotation: 15,
-        ease: Expo.easeOut
+        ease: Expo.easeOut,
       },
       0.2
     )
     .staggerTo(
-      ".idea-6 span",
+      '.idea-6 span',
       0.8,
       {
         scale: 3,
         opacity: 0,
         rotation: -15,
-        ease: Expo.easeOut
+        ease: Expo.easeOut,
       },
       0.2,
-      "+=1"
+      '+=1'
     )
     .staggerFromTo(
-      ".baloons img",
+      '.baloons img',
       2.5,
       {
         opacity: 0.9,
-        y: 1400
+        y: 1400,
       },
       {
         opacity: 1,
-        y: -1000
+        y: -1000,
       },
       0.2
     )
     .from(
-      ".lydia-dp",
+      '.lydia-dp',
       0.5,
       {
         scale: 3.5,
         opacity: 0,
         x: 25,
         y: -25,
-        rotationZ: -45
+        rotationZ: -45,
       },
-      "-=2"
+      '-=2'
     )
-    .from(".hat", 0.5, {
+    .from('.hat', 0.5, {
       x: -100,
       y: 350,
       rotation: -180,
-      opacity: 0
+      opacity: 0,
     })
     .staggerFrom(
-      ".wish-hbd span",
+      '.wish-hbd span',
       0.7,
       {
         opacity: 0,
         y: -50,
         // scale: 0.3,
         rotation: 150,
-        skewX: "30deg",
-        ease: Elastic.easeOut.config(1, 0.5)
+        skewX: '30deg',
+        ease: Elastic.easeOut.config(1, 0.5),
       },
       0.1
     )
     .staggerFromTo(
-      ".wish-hbd span",
+      '.wish-hbd span',
       0.7,
       {
         scale: 1.4,
-        rotationY: 150
+        rotationY: 150,
       },
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
-        ease: Expo.easeOut
+        color: '#ff69b4',
+        ease: Expo.easeOut,
       },
       0.1,
-      "party"
+      'party'
     )
     .from(
-      ".wish h5",
+      '.wish h5',
       0.5,
       {
         opacity: 0,
         y: 10,
-        skewX: "-15deg"
+        skewX: '-15deg',
       },
-      "party"
+      'party'
     )
     .staggerTo(
-      ".eight svg",
+      '.eight svg',
       1.5,
       {
-        visibility: "visible",
+        visibility: 'visible',
         opacity: 0,
         scale: 80,
         repeat: 3,
-        repeatDelay: 1.4
+        repeatDelay: 1.4,
       },
       0.3
     )
-    .to(".six", 0.5, {
+    .to('.six', 0.5, {
       opacity: 0,
       y: 30,
-      zIndex: "-1"
+      zIndex: '-1',
     })
-    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .staggerFrom('.nine p', 1, ideaTextTrans, 1.2)
     .to(
-      ".last-smile",
+      '.last-smile',
       0.5,
       {
-        rotation: 90
+        rotation: 90,
       },
-      "+=1"
+      '+=1'
     );
 
   // tl.seek("currentStep");
   // tl.timeScale(2);
 
   // Restart Animation on click
-  const replyBtn = document.getElementById("replay");
-  replyBtn.addEventListener("click", () => {
+  const replyBtn = document.getElementById('replay');
+  replyBtn.addEventListener('click', () => {
     tl.restart();
   });
 };
 
 // Import the data to customize and insert them into page
 const fetchData = () => {
-  fetch("customize.json")
-    .then(data => data.json())
-    .then(data => {
-      Object.keys(data).map(customData => {
-        if (data[customData] !== "") {
-          if (customData === "imagePath") {
+  fetch('customize.json')
+    .then((data) => data.json())
+    .then((data) => {
+      Object.keys(data).map((customData) => {
+        if (data[customData] !== '') {
+          if (customData === 'imagePath') {
             document
               .getElementById(customData)
-              .setAttribute("src", data[customData]);
+              .setAttribute('src', data[customData]);
           } else {
             document.getElementById(customData).innerText = data[customData];
           }
@@ -299,8 +298,34 @@ const fetchData = () => {
 const resolveFetch = () => {
   return new Promise((resolve, reject) => {
     fetchData();
-    resolve("Fetch done!");
+    resolve('Fetch done!');
   });
 };
 
-resolveFetch().then(animationTimeline());
+$('document').ready(function () {
+  const $btn = $('#turn-on-btn');
+  $btn.click(function () {
+    $('#bulb_yellow').addClass('bulb-glow-yellow');
+    $('#bulb_red').addClass('bulb-glow-red');
+    $('#bulb_blue').addClass('bulb-glow-blue');
+    $('#bulb_green').addClass('bulb-glow-green');
+    $('#bulb_pink').addClass('bulb-glow-pink');
+    $('#bulb_orange').addClass('bulb-glow-orange');
+    $('body').addClass('peach');
+    $(this)
+      .fadeOut('slow')
+      .delay(5000)
+      .promise()
+      .then(() => resolveFetch())
+      .then(() => animationTimeline());
+
+    setTimeout(() => {
+      $('#bulb_yellow').addClass('bulb-glow-yellow-after');
+      $('#bulb_red').addClass('bulb-glow-red-after');
+      $('#bulb_blue').addClass('bulb-glow-blue-after');
+      $('#bulb_green').addClass('bulb-glow-green-after');
+      $('#bulb_pink').addClass('bulb-glow-pink-after');
+      $('#bulb_orange').addClass('bulb-glow-orange-after');
+    }, 5000);
+  });
+});
